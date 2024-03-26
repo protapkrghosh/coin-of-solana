298bd67bfb22b4152488f0bf5146d9de769e655b
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-26 12:29:29 +0600

    aa

--- src/components/sections/Ecosystem/Ecosystem.jsx
+++ src/components/sections/Ecosystem/Ecosystem.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import Container from '../Container/Container';
 import ecoBgImgleft from '@/assets/ecosystem/Designerpre1.png';
 import ecoBgImgright from '@/assets/ecosystem/Designerpre2.png';
+import ecoBgImg from '@/assets/ecosystem/Group 1171275157.png';
 
 
 const Ecosystem = () => {
   return (
     <>
       <Container>
+
         <div>
           <p className='text-[#FFF] text-[66px] leading-[79.2px] text-center'>ECOSYSTEM</p>
+        </div>
+
+        <div>
+          <img src={ecoBgImg} alt="" />
+          <div>
+            <p className='text-[#FFF] text-[20px] leading-[32px]'>DEX</p>
+
+          </div>
         </div>
       </Container>
     </>
