a3953118d6d47533604161fd12317249217eb8e6
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-26 18:32:06 +0600

    aa

--- src/components/sections/Ecosystem/Ecosystem.jsx
+++ src/components/sections/Ecosystem/Ecosystem.jsx
@@ -17,9 +17,40 @@ import ecoBgImgrightIcon5 from '@/assets/ecosystem/Group 1171275169.png';
 const Ecosystem = () => {
   return (
     <>
-      <div>
+      <div className='bg-gradient-to-r from-[#191610] to-[#191610]'>
         <Container>
           <div>
+            <p className='text-[#FFF] text-[66px] leading-[79.2px] text-center'>ECOSYSTEM</p>
+
+            <div>
+              <img className='mt-[162px] 2xl:w-[80%] mx-auto' src={ecoBgImg} alt="image" />
+
+              <div className='relative'>
+                {/* left side card 2 */}
+                <div className='2xl:max-w-[101.092px] 2xl:bottom-56 2xl:left-[-95px]'>
+                  <p className='text-[#FFF] text-[20px] leading-[32px] text-center mb-[15px]'>Gaming</p>
+                  <div className='ecoborderleft flex justify-center items-center'>
+                    <img className='2xl:w-auto' src={ecoBgImgleftIcon2} alt="" />
+                  </div>
+                </div>
+              </div>
+
+
+              <div className='relative'>
+                {/* right side bar card 4 */}
+                <div className='2xl:absolute 2xl:right-[-250px] 2xl:bottom-[93px]'>
+                  <div className='ecoborderRight flex items-center gap-4 pl-[12px]'>
+                    <div className='ecoborderRightIcon flex justify-center items-center'>
+                      <img className='2xl:w-auto' src={ecoBgImgrightIcon4} alt="" />
+                    </div>
+                    <div>
+                      <p className='text-[#FFF] text-[20px] leading-[32px]'>NFT</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
 
           </div>
         </Container>
