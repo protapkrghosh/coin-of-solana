396281985ea08a5c9f833d9ef1dc4d5fa88af8c2
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-26 12:50:45 +0600

    aa

--- src/components/sections/Ecosystem/Ecosystem.jsx
+++ src/components/sections/Ecosystem/Ecosystem.jsx
@@ -3,27 +3,32 @@ import Container from '../Container/Container';
 import ecoBgImgleft from '@/assets/ecosystem/Designerpre1.png';
 import ecoBgImgright from '@/assets/ecosystem/Designerpre2.png';
 import ecoBgImg from '@/assets/ecosystem/Group 1171275157.png';
+import ecoBgImgleftIcon1 from '@/assets/ecosystem/Group 1171275165.png';
 
 
 const Ecosystem = () => {
   return (
     <>
       <Container>
-
         <div>
           <p className='text-[#FFF] text-[66px] leading-[79.2px] text-center'>ECOSYSTEM</p>
-        </div>
-
-        <div>
-          <img src={ecoBgImg} alt="" />
-          <div>
-            <p className='text-[#FFF] text-[20px] leading-[32px]'>DEX</p>
-
-          </div>
-        </div>
+        </div> 
       </Container>
     </>
   );
 };
 
 export default Ecosystem;
+
+
+
+
+
+
+
+{/* <div className='max-w-[101.092px]'>
+  <p className='text-[#FFF] text-[20px] leading-[32px] text-center mb-[15px]'>DEX</p>
+  <div className='ecoborderleft flex justify-center items-center'>
+    <img className='' src={ecoBgImgleftIcon1} alt="" />
+  </div>
+</div> */}
