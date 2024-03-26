962f06ee9a7ee82dbd8c090b9d5272a2d07c02f7
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-26 13:15:00 +0600

    aa

--- src/components/sections/Ecosystem/Ecosystem.jsx
+++ src/components/sections/Ecosystem/Ecosystem.jsx
@@ -10,9 +10,25 @@ const Ecosystem = () => {
   return (
     <>
       <Container>
-        <div>
-          <p className='text-[#FFF] text-[66px] leading-[79.2px] text-center'>ECOSYSTEM</p>
-        </div> 
+        <div className='border border-white 2xl:relative'>
+          <div>
+            <p className='text-[#FFF] text-[66px] leading-[79.2px] text-center'>ECOSYSTEM</p>
+          </div>
+
+          <div>
+            <div className='flex justify-evenly'>
+              <img className='2xl:mt-[162px]' src={ecoBgImg} alt="" />
+            </div>
+
+            <div className='2xl:max-w-[101.092px] 2xl:absolute 2xl:-bottom-10 2xl:left-7'>
+              <p className='text-[#FFF] text-[20px] leading-[32px] text-center mb-[15px]'>DEX</p>
+              <div className='ecoborderleft flex justify-center items-center'>
+                <img className='2xl:w-auto' src={ecoBgImgleftIcon1} alt="" />
+              </div>
+            </div>
+
+          </div>
+        </div>
       </Container>
     </>
   );
@@ -26,9 +42,9 @@ export default Ecosystem;
 
 
 
-{/* <div className='max-w-[101.092px]'>
+<div className='max-w-[101.092px]'>
   <p className='text-[#FFF] text-[20px] leading-[32px] text-center mb-[15px]'>DEX</p>
   <div className='ecoborderleft flex justify-center items-center'>
     <img className='' src={ecoBgImgleftIcon1} alt="" />
   </div>
-</div> */}
+</div>
