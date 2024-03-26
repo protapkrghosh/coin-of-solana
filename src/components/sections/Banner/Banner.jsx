9c6f17097513ffb69ec6be2abe99bfbbff26c35a
Protap Ghosh <protapg518@gmail.com>
2024-03-26 20:49:58 +0600

    banner completed

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -8,15 +8,15 @@ const Banner = () => {
   return (
     <div className='bg-gradient-to-r from-[#191610] to-[#191610]'>
       <Container>
-        <div style={{ backgroundImage: `url(${forgeImg})` }} className='bg-no-repeat bg-left pt-52 pb-16 -mt-[125px]'>
-          <div className='md:flex justify-between items-center gap-0'>
-            <div className='w-1/2 lg:w-[60%] xl:w-[55%] 2xl:w-[52%]'>
+        <div style={{ backgroundImage: `url(${forgeImg})` }} className='bg-no-repeat bg-left pt-52 pb-16 -mt-[140px] lg:-mt-[125px]'>
+          <div className='flex flex-col-reverse lg:flex-row justify-between items-center gap-0'>
+            <div className='w-full lg:w-[60%] xl:w-[55%] 2xl:w-[52%] mt-10 lg:mt-0'>
               <div>
-                <p className='text-white text-[50px] xl:text-[56px] 2xl:text-[64px] font-normal -mb-5'>The Community</p>
+                <p className='text-white text-[34px] md:text-[50px] xl:text-[56px] 2xl:text-[64px] font-normal -mb-2 md:-mb-5'>The Community</p>
 
-                <h1 className='text-[50px] lg:text-[48px] xl:text-[56px] 2xl:text-[64px] font-bold'><span className="bg-clip-text text-transparent bg-gradient-to-r from-[#D1CB18] to-[#E77962]">Coin of Solana </span></h1>
+                <h1 className='text-[34px] md:text-[50px] lg:text-[48px] xl:text-[56px] 2xl:text-[64px] font-bold'><span className="bg-clip-text text-transparent bg-gradient-to-r from-[#D1CB18] to-[#E77962]">Coin of Solana </span></h1>
 
-                <p className='text-[#CBC2C2] xl:text-[18px] font-normal mt-5 xl:mt-8 mb-8 xl:mb-14'>The new token PERRY v2 is here to make memecoins great again. The most memeable memecoin in existence. Tje PEPE have had their day, it’s time for PERRY to take reign!</p>
+                <p className='text-[#CBC2C2] text-[15px] lg:text-[16px] xl:text-[18px] font-normal mt-5 xl:mt-8 mb-8 xl:mb-14'>The new token PERRY v2 is here to make memecoins great again. The most memeable memecoin in existence. Tje PEPE have had their day, it’s time for PERRY to take reign!</p>
 
                 <div className='w-fit flex gap-x-5 xl:gap-x-8'>
                   <a href="" className='w-[70px] h-[70px]'>
@@ -41,8 +41,8 @@ const Banner = () => {
             </div>
 
             {/* Banner Image */}
-            <div className='w-1/2 lg:w-[40%] xl:w-[45%] 2xl:w-[48%] lg:-mr-10 xl:-mr-16 2xl:-mr-10'>
-              <img src={bannerImg} alt="Image" className='w-[400px] xl:w-[500px] 2xl:w-auto 2xl:scale-105' />
+            <div className='w-full lg:w-[40%] xl:w-[45%] 2xl:w-[48%] lg:-mr-10 xl:-mr-16 2xl:-mr-10'>
+              <img src={bannerImg} alt="Image" className='w-[400px] xl:w-[500px] 2xl:w-auto 2xl:scale-105 mx-auto lg:mx-0' />
             </div>
           </div>
         </div>
