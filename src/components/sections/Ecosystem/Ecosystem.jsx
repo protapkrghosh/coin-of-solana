641cfc2d40a0b0d72f0a080560abbfb5f4ba8a95
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-26 12:02:25 +0600

    aa

--- src/components/sections/Ecosystem/Ecosystem.jsx
+++ src/components/sections/Ecosystem/Ecosystem.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Container from '../Container/Container';
 
 const Ecosystem = () => {
-    return (
+  return (
+    <>
+      <Container>
         <div>
-            <p>hello man</p>
+          <p className='text-[#FFF] text-[66px] leading-[79.2px] text-center'>ECOSYSTEM</p>
         </div>
-    );
+      </Container>
+    </>
+  );
 };
 
 export default Ecosystem;
